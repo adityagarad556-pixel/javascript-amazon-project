3339e316fb76252a8ebc10c226c917889d5cdca5
aditya <adityagarad556@gmail.com>
2026-09-07 22:18:01 +0530

    deleting product form HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem)=>{
 
 console.log(matchingProduct);
   cartSummaryHTML +=  `
-     <div class="cart-item-container">
+     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -40,7 +40,7 @@ console.log(matchingProduct);
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -102,6 +102,12 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 link.addEventListener('click',()=>{
  const productId = link.dataset.productId;
  removeFromCart(productId);
-console.log(cart);
+
+ const container = document.querySelector(
+  `.js-cart-item-container-${productId}`
+ );
+
+ container.remove();
+
 });
 });
