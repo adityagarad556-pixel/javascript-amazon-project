32b537f38906cab1c6152d76ef24038937912e91
aditya <adityagarad556@gmail.com>
2026-09-18 18:12:27 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,64 @@ import { renderpaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 import { loadProducts } from "../data/products.js";
+import {loadCart} from '../data/cart.js';
 
-loadProducts(()=>{
+// new Promise((resolve)=>{
+//    loadProducts(()=>
+//     {
+//         resolve();
+//     });
+// }).then(()=>
+//     {
+//     renderOrderSummary();
+//     renderpaymentSummary();
+// });
+
+//Promise() used to run the code imemediately
+//resolve: is a function which control when to go to next step
+
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve('value2');
+        });
+    })
+
+]).then((values)=>{
+    console.log(values)
     renderOrderSummary();
     renderpaymentSummary();
 });
+
+
+// new Promise((resolve)=>{
+//     loadProducts(()=>{
+//         resolve('value1');
+//     });
+
+// }).then((value)=>{
+//     console.log(value);
+
+//     return new Promise((resolve)=>{
+//         loadCart(()=>{
+//             resolve();
+//         });
+//     });
+
+// }).then(()=>{
+//     renderOrderSummary();
+//     renderpaymentSummary();
+// });
+
+// loadProducts(()=>{
+//  loadCart(()=>{
+//         renderOrderSummary();
+//         renderpaymentSummary();
+//     });
+   
+// });
