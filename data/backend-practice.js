7158dcaefb9ded1985a69e58c120d5ba89a4fa0b
aditya <adityagarad556@gmail.com>
2026-09-18 11:32:24 +0530

    add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest();//build in js class to send the request to backend data
+
+xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+});
+
+xhr.open('GET','https://supersimplebackend.dev');
+//GET is for , get some information from the backend
+// URL for where to send the massage
+xhr.send();
+xhr.response
